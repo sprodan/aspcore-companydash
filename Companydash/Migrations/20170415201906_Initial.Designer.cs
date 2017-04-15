219fa46398d6c554deb404f44a2f56d01f3a3e75
Продан Станислав Анатолиевич <sprodan8@gmail.com>
2017-04-15 23:21:53 +0300

    Added DB Added Models Added MVC

---
+++ Companydash/Migrations/20170415201906_Initial.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Companydash.Models;
+
+namespace Companydash.Migrations
+{
+    [DbContext(typeof(MobileContext))]
+    [Migration("20170415201906_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Companydash.Models.Order", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Adress");
+
+                    b.Property<string>("ContactPhone");
+
+                    b.Property<int>("PhoneId");
+
+                    b.Property<string>("User");
+
+                    b.HasKey("OrderId");
+
+                    b.HasIndex("PhoneId");
+
+                    b.ToTable("Orders");
+                });
+
+            modelBuilder.Entity("Companydash.Models.Phone", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Company");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Price");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Phones");
+                });
+
+            modelBuilder.Entity("Companydash.Models.Order", b =>
+                {
+                    b.HasOne("Companydash.Models.Phone", "Phone")
+                        .WithMany()
+                        .HasForeignKey("PhoneId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
